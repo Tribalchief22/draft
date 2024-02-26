bb7748fdc3452a26533645bbcc7c86e09c10d47c
Tribalchief22 <135589141+Tribalchief22@users.noreply.github.com>
2024-02-26 20:11:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let startX = 0;
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.image');
-  
-    function handleTouchStart(event) {
-      startX = event.touches[0].clientX;
-    }
-  
-    function handleTouchMove(event) {
-      const xDiff = startX - event.touches[0].clientX;
-  
-      if (xDiff > 50) {
-        showSlide(currentSlide + 1);
-      } else if (xDiff < -50) {
-        showSlide(currentSlide - 1);
-      }
-    }
-  
-    function showSlide(slideIndex) {
-      if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-      } else if (slideIndex >= slides.length) {
-        slideIndex = 0;
-      }
-  
-      slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-          slide.classList.add('active');
-        } else {
-          slide.classList.remove('active');
-        }
-      });
-  
-      currentSlide = slideIndex;
-    }
-  
-    document.querySelector('.gallery').addEventListener('touchstart', handleTouchStart);
-    document.querySelector('.gallery').addEventListener('touchmove', handleTouchMove);
-  
-    // Play audio on image click
-    slides.forEach(slide => {
-      slide.addEventListener('click', function() {
-        const audioFile = this.getAttribute('data-audio');
-        const audio = new Audio(audioFile);
+  const sounds = document.querySelectorAll('.sound');
+
+  sounds.forEach(sound => {
+    const audio = sound.querySelector('audio');
+    let isPlaying = false;
+
+    sound.addEventListener('click', function() {
+      if (!isPlaying) {
+        audio.currentTime = 0;
         audio.play();
-      });
+      } else {
+        audio.pause();
+      }
+      isPlaying = !isPlaying;
+    });
+
+    audio.addEventListener('ended', function() {
+      isPlaying = false;
     });
   });
+});
+
   
